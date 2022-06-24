7b1370c01e6c4a23b821fada7bbf22749ad9e97a
Ryman56 <erickochieng830@gmail.com>
2022-06-24 12:31:22 +0300

    Create about-me.js

---
+++ about-me.js
@@ -0,0 +1,4 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+console.log('Otieno Erick Ochieng')
+console.log('Height: 176cm');
+console.log('Country: Kenya');
